0709f27cf8f4d3f5be011086fe1fa7218f416ae2
Zaid <zaidsheikh2704@gmail.com>
2025-08-22 14:12:04 +0530

    Initial commit

---
+++ instruction.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Guide manual</title>
+    <meta name='viewport' content='width=device-width, initial-sScale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='index.css'>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src='main.js'></script>
+</head>
+
+<body>
+    <div class="second" id="about">
+        <br><br>
+        <h1>Voting Guidelines</h1>
+        <br><br>
+
+        <p>An online voting system is a software platform that allows groups to securely conduct votes. High-quality
+            online voting systems
+            balance ballot security, accessibility, and the overall requirements of an organization's voting
+            event.<br><br>
+            At their core, online voting systems protect the integrity of your vote by preventing voters from being able
+            to vote multiple times.
+            As a digital platform, they eliminate the need to gather in-person, cast votes using paper, or by any other
+            means
+            (e.g. email, insecure survey software).<br><br>
+            You may hear an online voting system being referred to as an online e- voting system, or electronic voting.
+            These all make reference to the same thing: a secure voting tool that allows your group to collect input
+            from your group
+            and closely scrutinize the results in real time.<br><br><br><br>
+            Online voting : Safest way to held Small level Elections in the Pandemic (COVID-19).
+        </p><br><br>
+        <!-- <a href="Register.php" id="btn">Register</a> -->
+        <div class="w-full text-center">
+            <a class="btn btn-warning" href="login.php">Continue</a>
+        </div>
+
+    </div>
+
+    <!-- Footer ======================== -->
+</body>
+
+</html>
---
+++ policy.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Security Policy</title>
+    <meta name='viewport' content='width=device-width, initial-sScale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='index.css'>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src='main.js'></script>
+        
+</head>
+
+<body>
+    <div class="second" id="about">
+        <br><br>
+        <h1>Privacy Policy</h1>
+        <h2>
+            &quot; Cookie Policy &quot;
+        </h2>
+        <br><br>
+        <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
+<p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
+<p>While using Our Service, We may ask You to provide Us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information include :</p>
+<ul>
+<li>
+<p>Email address</p>
+</li>
+<li>
+<p>First name and last name</p>
+</li>
+<li>
+<p>Phone number</p>
+</li>
+<p>The Company will retain Your Personal Data only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use Your Personal Data to the extent necessary to comply with our legal obligations (for example, if we are required to retain your data to comply with applicable laws), resolve disputes, and enforce our legal agreements and policies.</p>
+<br><br>
+         <!-- <a href="Register.php" id="btn">Register</a> -->
+         <div class="w-full text-center">
+            <a class="btn btn-warning" href="instruction.php">I AGREE</a>
+        </div>
+
+    </div>
+
+    <!-- Footer ======================== -->
+    <footer>
+        <center>
+            <div class="sociallogo" id="contact">
+                <a href="#"><i class="fab fa-facebook fa-2x"></i></a>
+                <a href="#"><i class="fab fa-instagram fa-2x"></i></a>
+                <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
+                <a href="#"><i class="fab fa-linkedin fa-2x"></i></a>
+            </div>
+            <address>
+                <br>
+                <h4>Contact</h4>
+                &quot; SYMBIOSIS INSTITUTE OF TECHNOLOGY , NAGPUR &quot;<br><br>
+                sitnagpur@gmail.com<br>
+                supportsit@vote.com<br>
+                999XXXXXX
+            </address> <br>
+            &copy Copyright 2021 by SIT NAGPUR. All Right Reserved
+        </center>
+    </footer>
+
+ 
+</body>
+
+</html>
